cf9cbc103126783608f356fe7748fbbbaaf4d49f
Lich2013 <longzy2013@gmail.com>
2016-06-02 22:01:55 +0800

    index working

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,14 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-       $this->display();
+        $type = M('type');
+        $articleCategory = $type->where(array('type' => 'article', 'status' => 1))->select();
+        $article = M('article');
+        foreach ($articleCategory as $value) {
+            $articlelist[] = $article->where(array('type_id' => $value['id']))->order('id desc')->limit(6)->select();
+        }
+        
+        $this->assign('articlelist', $articlelist);
+        $this->display();
     }
 }
