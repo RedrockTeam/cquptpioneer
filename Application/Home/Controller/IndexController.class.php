4e1de9425587ab372e87e31211991592f8f69c82
Lich2013 <longzy2013@gmail.com>
2016-06-02 22:06:36 +0800

    articlelist ok

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -2,15 +2,24 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
+
+    private $type;
+    private $article;
+
     public function index(){
-        $type = M('type');
-        $articleCategory = $type->where(array('type' => 'article', 'status' => 1))->select();
-        $article = M('article');
-        foreach ($articleCategory as $value) {
-            $articlelist[] = $article->where(array('type_id' => $value['id']))->order('id desc')->limit(6)->select();
-        }
-        
+        $this->type = M('type');
+        $this->article = M('article');
+
+        $articlelist = $this->articlelist();
         $this->assign('articlelist', $articlelist);
         $this->display();
+    }
+
+    private function articlelist() {
+        $articleCategory = $this->type->where(array('type' => 'article', 'status' => 1))->select();
+        foreach ($articleCategory as $value) {
+            $articlelist[] = $this->article->where(array('type_id' => $value['id']))->order('id desc')->limit(6)->select();
+        }
+        return $articlelist;
     }
 }
