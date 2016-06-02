eb65c221a96da6fe72231c1ab08ad8b7b7e4f1e3
Lich2013 <longzy2013@gmail.com>
2016-06-02 17:20:29 +0800

    path

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,6 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        hash('sha256', '123456');
+       $this->display();
     }
 }
