d4697fa234fe9068833df86932797fd2abe575d7
Lich2013 <longzy2013@gmail.com>
2016-06-07 18:02:17 +0800

    test

--- Public/vendors/ckeditor/config.js
+++ Public/vendors/ckeditor/config.js
@@ -6,7 +6,7 @@
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+	config.uiColor = '#AADC6E';
     config.pasteFromWordNumberedHeadingToList = true;
     config.pasteFromWordRemoveStyles = false;
     config.pasteFromWordRemoveFontStyles = false;
