89b94b0a02c113d31136468b0e61e1f0d2b3bce2
Lich2013 <longzy2013@gmail.com>
2016-06-15 17:07:02 +0800

    debug

--- Application/Home/Controller/ArticleController.class.php
+++ Application/Home/Controller/ArticleController.class.php
@@ -16,14 +16,20 @@ class ArticleController extends BaseController {
     public function mobilearticle(){
         $id = I('post.id');
         $data = M('article')->where(array('id' => $id))->find();
-        $data['file'] = array(
-            array(
-                'name' => $data['file_name'],
-                'address' => $data['file_path']
+        if ($data['file_name']) {
+            $data['file'] = array(
+                array(
+                    'name' => $data['file_name'],
+                    'address' => $data['file_path']
                 )
-        );
-        unset($data['file_name']);
-        unset($data['file_path']);
+            );
+            unset($data['file_name']);
+            unset($data['file_path']);
+        } else {
+            unset($data['file_name']);
+            unset($data['file_path']);
+            $data['file'] = [];
+        }
         $this->ajaxReturn(array(
             'status' => 200,
             'info'   => 'success',
