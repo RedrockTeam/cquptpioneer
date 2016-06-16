6a1259bb82b4c3937310c00a03dfe448b6c8daf1
Lich2013 <longzy2013@gmail.com>
2016-06-16 13:49:50 +0800

    test

--- Application/Home/Controller/LoginController.class.php
+++ Application/Home/Controller/LoginController.class.php
@@ -14,8 +14,10 @@ class LoginController extends BaseController {
             ));
         }
         $password = sha1($password);
+        var_dump($password);
         $users = M('users');
         $data = $users->where(array('idcard' => $idcard, 'password' => $password))->find();
+        var_dump($data);
         if ($data == '' || $data == null) {
             $this->ajaxReturn(array(
                 'status' => 403,
